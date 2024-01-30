f2045bcc85db8d2f1e47c1444939d70597c40115
cringeyburger <medaankit18@gmail.com>
2024-01-31 04:02:25 +0530

    feat: stylized textbox and added "link" icon

--- Frontend/src/components/textbox/textbox.tsx
+++ Frontend/src/components/textbox/textbox.tsx
@@ -1,11 +1,16 @@
-import React from "react";
+"use client";
+import { FaLink } from "react-icons/fa";
 import "../../styles/Textbox.scss";
 
 const Textbox = () => {
   return (
     <div className="center-wrapper">
       <div className="textbox-container">
-        <input className="" placeholder="Paste URL" />
+        <FaLink className="icon" />
+        <form>
+          <input type="text" placeholder="Enter a link to shorten it" />
+          <button type="submit">Shorten URL</button>
+        </form>
       </div>
     </div>
   );
