529645891467df7dbdc890101aba4246997d2250
cringeyburger <medaankit18@gmail.com>
2024-01-31 04:41:08 +0530

    feat: improved UI of redirecting page

--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -1,15 +1,18 @@
 import type { Metadata } from "next";
 import Textbox from "../components/textbox/textbox";
+import "../styles/page.scss";
 
 export const metadata: Metadata = {
-  title: "URL Shortener",
-  description: "Website to shorten URL for convenience",
+  title: "ZipURL - A Fast and Efficient Link Shortener",
+  description:
+    "Simple, Fast and Free URL Shortener. Track link performance, history and more. Team up with your friends and colleagues.",
 };
 
 export default function Page() {
   return (
-    <div>
-      <h1>Redirecting index page goes here</h1>
+    <div className="page-container">
+      <h1 className="underline">Empower your links, Embrace Simplicity.</h1>
+      <h2>ZipURL - Where Every Link Finds Its Shortcut</h2>
       <Textbox />
     </div>
   );
--- Frontend/src/components/navbar.tsx
+++ Frontend/src/components/navbar.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Navbar(params: { logged_in: boolean }) {
   return (
     <div id="navbar">
-      <p>URL Shortener</p>
+      <p>ZipURL</p>
       <div>
         <Link className="link" href="/teams">
           Teams
