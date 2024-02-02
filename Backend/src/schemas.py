3c4bdfb7788c7887cc7c54e7114936354417896d
Lightening_Panda <shrishtripathi@Shrishs-MacBook-Air.local>
2024-02-02 19:15:22 +0530

    urllogic

--- Backend/src/schemas.py
+++ Backend/src/schemas.py
@@ -2,6 +2,8 @@ from pydantic import BaseModel, EmailStr, constr, HttpUrl, conint, validator, Fi
 from typing import List, Optional, Union
 from typing import List, Optional, Union
 from enum import Enum
+from pydantic import BaseModel
+
 
 class LoginUserSchema(BaseModel):
     email: EmailStr
@@ -11,3 +13,7 @@ class SignUPUserSchema(LoginUserSchema):
     username: constr(min_length=3, max_length=50)
     email: EmailStr
     password: constr(min_length=8, max_length=128)
+    
+class ShortenedUrl(BaseModel):
+    short_url: str
+    original_url: str
